a1cf8d0f555f84d5b3dd347144d5246a77678c23
Eric-Obeng <obengeric.3958@gmail.com>
2022-09-09 10:07:44 -0700

    Task-6 Done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ * Return: 0 when successful
+ * Description - All your code should be in the main function
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= 9; d++)
+	{
+		putchar(d);
+		return (0);
+	}
+}
