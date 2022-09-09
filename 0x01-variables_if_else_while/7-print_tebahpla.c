7a833434fc55040098fc7ff6a64f51089bb64351
Eric-Obeng <obengeric.3958@gmail.com>
2022-09-09 09:21:57 -0700

    Task-7.Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets reversely
+ * Return: 0
+ * Description - All your code should be in the main function
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
