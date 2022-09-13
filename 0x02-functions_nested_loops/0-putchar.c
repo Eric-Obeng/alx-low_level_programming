e95ee9086f644a82b8e0913644b1805a219b7865
Eric-Obeng <obengeric.3958@gmail.com>
2022-09-13 04:49:19 -0700

    Task-0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: 0 when successful
+ * Description - The program should return 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+}
