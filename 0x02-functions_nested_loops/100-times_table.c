b5ee75804f81b1bc878aa176cfd8cbbb168bacb0
Eric-Obeng <obengeric.3958@gmail.com>
2022-09-14 16:46:35 -0700

    Task 12 modified

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,13 +6,12 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+	k = j * i;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			k = j * i;
-
 			if (j == 0)
 			{
 				_putchar(k + '0');
