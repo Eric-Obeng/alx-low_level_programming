03d935326ea081424cf6f869e405c79867001120
Eric-Obeng <obengeric.3958@gmail.com>
2022-09-15 07:44:09 -0700

    main.h modified

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -20,4 +16,4 @@ int mul(int, int);
 
 
 
-#endif /* MAIN_H */
+#endif
